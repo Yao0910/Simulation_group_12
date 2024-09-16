8a4893acefd4d0d06cc6bb110b2fdc9ebf5b6d32
SmileFisch <jincpan@gmail.com>
2024-09-16 18:28:17 +0200

    modify Fehler in Aufgabe 4.3.3“

--- Versuch_1_Gruppen_12/4.3/Aufgabe_4_3_3_Parameter_Drehtellers.m
+++ Versuch_1_Gruppen_12/4.3/Aufgabe_4_3_3_Parameter_Drehtellers.m
@@ -1,8 +1,17 @@
-PHI = 0.001; %Hauptfluss im Elektromotor
-U = 24; %[V] angelegte Spannung am Elektromotor
-Ra = 40; %[Ohm] Ankerwiderstand des Elektromotors
-I = 0.5; %[kgm²] Massenträgheit des Tellers
-K = 8; %Wicklungsanzahl
-MRK = 1; %Konstantes Reibungsmoment
-A = 5; %Konstante für geschwindigkeitsabhängige Reibung
-igetr = 1080; %Getriebeübersetzung
+%Parameter Definition
+%Hauptfluss im Elektromotor
+PHI = 0.001; 
+%[V] angelegte Spannung am Elektromotor
+U = 24; 
+%[Ohm] Ankerwiderstand des Elektromotors
+Ra = 40; 
+%[kgm²] Massenträgheit des Tellers
+I = 0.5; 
+%Wicklungsanzahl
+K = 8; 
+%Konstantes Reibungsmoment
+MRK = 1; 
+%Konstante für geschwindigkeitsabhängige Reibung
+A = 5; 
+%Getriebeübersetzung
+igetr = 1080; 
