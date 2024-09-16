5fdd594648df2954939dc031f69b7bea657f86de
Yao <larryadieu@163.com>
2024-09-16 17:58:37 +0200

    4.3.3.parameter

---
+++ Versuch_1_Gruppen_12/4.3/Aufgabe_4_3_3_Parameter_Drehtellers.m
@@ -0,0 +1,8 @@
+PHI = 0.001; %Hauptfluss im Elektromotor
+U = 24; %[V] angelegte Spannung am Elektromotor
+Ra = 40; %[Ohm] Ankerwiderstand des Elektromotors
+I = 0.5; %[kgm²] Massenträgheit des Tellers
+K = 8; %Wicklungsanzahl
+MRK = 1; %Konstantes Reibungsmoment
+A = 5; %Konstante für geschwindigkeitsabhängige Reibung
+igetr = 1080; %Getriebeübersetzung
